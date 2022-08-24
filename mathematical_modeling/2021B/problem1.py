102a67eb9e850256590f819fae99ad98ba979659
royal-dargon <2312749625@qq.com>
2022-08-24 21:33:41 +0800

    update:the func about ppw

--- mathematical_modeling/2021B/problem1.py
+++ mathematical_modeling/2021B/problem1.py
@@ -4,6 +4,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import Pipeline
 import numpy as np
+from scipy.stats import pearsonr
+
 
 
 # this is the names of data
@@ -14,6 +16,8 @@ temperatures_3 = [250, 275, 300, 325, 350, 400, 450]
 temperatures_4to5 = [250, 275, 300, 325, 350, 400]
 temperatures_6to16 = [250, 275, 300, 350, 400]
 temperatures_17to21 = [250, 275, 300, 325, 350, 400]
+# the name of excel2's list
+excel_2_name = ['乙醇转化率(%)', '乙烯选择性', 'C4烯烃选择性', '乙醛选择性', '碳数为4-12脂肪醇', '甲基苯甲醛和甲基苯甲醇']
 
 
 # get data from the excel
@@ -35,6 +39,36 @@ def get_data():
         all_eth.append(eth_conversion)
         all_c4.append(c4)
     return all_eth, all_c4
+
+
+# get data from excel2
+def get_data_2():
+    data = pd.read_excel("data2.xlsx", skiprows=[0, 1, 2], header=None)
+    list_time = []
+    list_1 = []
+    list_2 = []
+    list_3 = []
+    list_4 = []
+    list_5 = []
+    list_6 = []
+    for i in range(len(data)):
+        list_time.append(data.iloc[i, 0])
+        list_1.append(data.iloc[i, 1])
+        list_2.append(data.iloc[i, 2])
+        list_3.append(data.iloc[i, 3])
+        list_4.append(data.iloc[i, 4])
+        list_5.append(data.iloc[i, 5])
+        list_6.append(data.iloc[i, 6])
+    parameters = {
+        "list1": list_time,
+        "list2": list_1,
+        "list3": list_2,
+        "list4": list_3,
+        "list5": list_4,
+        "list6": list_5,
+        "list7": list_6
+    }
+    return data, parameters
 
 
 # this is the func of drawing the pictures about data
@@ -183,10 +217,48 @@ def fit_data(all_eth, all_c4):
             plt.show()
 
 
+# 手写皮尔威系数计算
+def ppw(x, y):
+    n = len(x)
+    sum_xy = np.sum(np.sum(x * y))
+    sum_x = np.sum(np.sum(x))
+    sum_y = np.sum(np.sum(y))
+    sum_x2 = np.sum(np.sum(x * x))
+    sum_y2 = np.sum(np.sum(y * y))
+    pcc = (n * sum_xy - sum_x * sum_y) / np.sqrt((n * sum_x2 - sum_x * sum_x) * (n * sum_y2 - sum_y * sum_y))
+    return pcc
+
+
+# 计算皮尔威系数
+def my_ppw(parameters):
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.rcParams['axes.unicode_minus'] = False
+    time_list = parameters['list1']
+    all_list = []
+    all_list.append(parameters['list2'])
+    all_list.append(parameters['list3'])
+    all_list.append(parameters['list4'])
+    all_list.append(parameters['list5'])
+    all_list.append(parameters['list6'])
+    all_list.append(parameters['list7'])
+    for i in range(len(all_list)):
+        for n in range(i+1, len(all_list)):
+            # num = ppw(np.array(all_list[i]), np.array(all_list[n]))
+            # res = np.corrcoef(all_list[i], all_list[n])
+            # res = pearsonr(all_list[i], all_list[n])
+            # print(str(excel_2_name[i]) + '与' + str(excel_2_name[n]) + '的皮威尔系数为 ' + str(num) + '。置信区间为' + str(res))
+            plt.scatter(all_list[i], all_list[n])
+            plt.xlabel(excel_2_name[i])
+            plt.ylabel(excel_2_name[n])
+            plt.show()
+
+
 def main():
-    all_eth, all_c4 = get_data()
-    print_plot(all_eth, all_c4)
-    fit_data(all_eth, all_c4)
+    # all_eth, all_c4 = get_data()
+    # print_plot(all_eth, all_c4)
+    # fit_data(all_eth, all_c4)
+    data2, parameters = get_data_2()
+    my_ppw(parameters)
 
 
 if __name__ == "__main__":
